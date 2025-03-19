25b2dbd90ac7378894fa83646a412eb7473e73b7
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2025-03-19 23:31:37 +0500

    Update task1.asm

--- lab3/task1.asm
+++ lab3/task1.asm
@@ -1,1 +1,50 @@
 
+TITLE lab3
+INCLUDE Irvine32.inc
+
+.data
+	val1 WORD ?
+	val2 SBYTE -11
+	val3 SDWORD -45
+	val4 WORD 100,200,300
+	color BYTE "Navy Blue",0
+	a  BYTE  11h
+    b  BYTE  10h
+    c  BYTE  30h
+    d  BYTE  40h
+	E BYTE 14
+	result DWORD ?
+    wArray BYTE 10,20,30
+    dArray DWORDS 50 DUP(?)
+    testString DWORD 500 DUP("test")
+    bArray BYTE 20 DUP(0)
+
+	
+.code
+	main PROC
+		movzx eax, a   
+        movzx ebx, b   
+
+        add eax, ebx   
+        mov edx, eax   
+
+        movzx eax, a    
+        movzx ebx, b    
+    
+        sub eax, ebx   
+    
+        sub edx, eax   
+
+        movzx eax, c 
+        add edx, eax   
+
+        movzx eax, d 
+        add edx, eax    
+
+        mov ebx, edx   
+        mov result, ebx  
+
+        call ExitProcess 	
+	main ENDP
+
+END main
