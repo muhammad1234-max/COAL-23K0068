67aada5a9cc5cc69ec8a91cae9ab3673dc8910ff
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2025-05-03 20:49:57 +0500

    Create q3.asm

---
+++ lab9/q3.asm
@@ -0,0 +1,53 @@
+.model small
+.stack 100h
+.data
+    arr db 25, 70, 33, 90, 60
+    maxVal db ?
+
+.code
+main:
+    mov ax, @data
+    mov ds, ax
+
+    mov cx, 5
+    mov si, 0
+
+    mov al, arr[si]     ; Assume first element is max
+    mov maxVal, al
+    inc si
+    dec cx              ; Already considered first element
+
+find_max:
+    mov al, arr[si]
+    cmp al, maxVal
+    jle skip
+    mov maxVal, al
+skip:
+    inc si
+    loop find_max
+
+    mov al, maxVal
+    call PrintNumber
+
+    mov ah, 4ch
+    int 21h
+
+PrintNumber proc
+    xor ah, ah
+    mov bx, 10
+    div bx
+    add al, '0'
+    push ax
+    mov al, ah
+    add al, '0'
+    mov dl, al
+    mov ah, 02h
+    int 21h
+    pop ax
+    mov dl, al
+    mov ah, 02h
+    int 21h
+    ret
+PrintNumber endp
+
+end main
